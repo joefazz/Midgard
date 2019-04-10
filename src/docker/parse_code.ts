827edee12ab5f7a4b04c255b75ffae35691abced
JoeFazz <joseph@fazzino.net>
2019-04-10 21:54:06 +0100

    Add useless function

--- src/docker/parse_code.ts
+++ src/docker/parse_code.ts
@@ -18,6 +18,26 @@ export function getCodeSaveCommand(filename: string, code: string) {
     return cmd;
 }
 
+// FIXME: this doesn't work but maybe it could?
+export function getCodeSaveAndRunCommand(
+    filename: string,
+    code: string,
+    repl: Repl
+) {
+    let command = ["/bin/bash", "-c"];
+
+    code = code.replace(/"/g, `\\"`);
+    code = code.replace(/`/g, "\\`");
+
+    command.push(`echo "${code}" > ${filename}`);
+
+    if (repl === Repl.C) {
+        return command.concat(`&& gcc ${filename} && ./a.out && rm a.out`);
+    } else {
+        return command.concat(`&& ${repl} ${filename}`);
+    }
+}
+
 export function getCodeExecutionCommand(repl: Repl, file: string) {
     let command = ["/bin/bash", "-c"];
 
